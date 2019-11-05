d8f9946ec0e03c4971484d09ba5a3694be36d1ff
Arcanis <arcanis.dev@gmail.com>
2019-11-05 16:11:30 +0100

    Début Projet
    Mise en place de la salle et de l'avatar

---
+++ Baballe/Assets/BallManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class BallManager : MonoBehaviour {
+    private Transform spawnPoint;
+    public GameObject baballe;
+    private Rigidbody ballRigidbody;
+
+    public float throwForce = 1f;
+    
+    public bool hasBall;
+
+    private void Awake() {
+        this.spawnPoint = this.transform.GetChild(1);
+        this.ballRigidbody = this.baballe.GetComponent<Rigidbody>();
+    }
+    
+    private void Update() {
+        if (Input.GetButton("Fire1")) {
+            if (hasBall) {
+                this.ThrowBall();
+            }
+        }
+        
+    }
+
+    public void ThrowBall() {
+        this.ballRigidbody.AddForce(transform.forward* this.throwForce, ForceMode.Impulse);
+    }
+}

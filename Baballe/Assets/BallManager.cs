3be12785a820d653d09e94dd4ee0707e44dc8266
Arcanis <arcanis.dev@gmail.com>
2019-11-19 14:43:18 +0100

    Meilleur throw + test coroutine

--- Baballe/Assets/BallManager.cs
+++ Baballe/Assets/BallManager.cs
@@ -2,32 +2,95 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.PlayerLoop;
 
 public class BallManager : MonoBehaviour {
-    private Transform spawnPoint;
+    private Transform hand;
     public GameObject baballe;
     private Rigidbody ballRigidbody;
+    private Collider ballCollider;
 
-    public float throwForce = 1f;
+    public float throwForceMin = 1f;
+    public float throwForceMax = 5f;
+    public float upForce = 1.2f;
+    public float throwAmount;
     
-    public bool hasBall;
+    public bool hasBall = true;
 
     private void Awake() {
-        this.spawnPoint = this.transform.GetChild(1);
+        this.hand = transform.Find("Hand");
         this.ballRigidbody = this.baballe.GetComponent<Rigidbody>();
+        this.ballRigidbody.position = this.hand.position;
+        this.ballRigidbody.useGravity = false;
+        this.ballCollider = baballe.GetComponent<Collider>();
+        EnableBall(false);
     }
     
     private void Update() {
-        if (Input.GetButton("Fire1")) {
+        if (Input.GetButtonDown("Fire1")){
             if (hasBall) {
+                //Start throwing
                 this.ThrowBall();
+                //StartCoroutine(this.TestCor());
             }
+            else if(!this.hasBall){
+                this.RecallBall();
+            }
+        }else if (Input.GetButton("Fire1")) {
+            
+        }else if (Input.GetButtonUp("Fire1")) {
+            if (this.hasBall) {
+                
+            }
+        }
+
+        Debug.Log("iskine" + this.ballRigidbody.isKinematic);
+        Debug.Log("usegrav" + this.ballRigidbody.useGravity);
+        
+    }
+
+    private IEnumerator TestCor() {
+        while (Input.GetButton("Fire1")) {
+            Debug.Log("test");
+        }
+        yield return null;
+    }
+
+    private void ThrowBall() {
+        float throwForce = this.throwForceMin;
+        Debug.Log("throw");
+
+        if(Input.GetButton("Fire1")) {
+            throwForce += throwAmount +Time.deltaTime;
+        }
+
+        if (Input.GetButtonUp("Fire1")) {
+            this.hasBall = false;
+            EnableBall(true);
+            this.ballRigidbody.AddForce(this.hand.forward * throwForce + this.hand.up * upForce, ForceMode.Impulse);
         }
         
     }
 
-    public void ThrowBall() {
-        this.ballRigidbody.AddForce(transform.forward* this.throwForce, ForceMode.Impulse);
+    private void FixedUpdate() {
+        if (this.hasBall) {
+            this.ballRigidbody.position = this.hand.position;
+            this.ballRigidbody.rotation = this.hand.rotation;
+        }
+    }
+
+    public void RecallBall() {
+        Debug.Log("recall");
+        //tp balle + "désactive" la balle
+        
+        this.hasBall = true;
+        this.EnableBall(false);
+        this.ballRigidbody.position = this.hand.position;
+        
+    }
+
+    public void EnableBall(bool isTrue) {
+        this.ballRigidbody.isKinematic = !isTrue;
+        this.ballRigidbody.useGravity = isTrue;
+        this.ballCollider.enabled = isTrue;
     }
 }
---
+++ Baballe/Assets/FollowCamera.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class FollowCamera : MonoBehaviour {
+    public Camera cam;
+    private void Start() {
+        cam = Camera.main;
+    }
+
+    private void FixedUpdate() {
+        transform.position = cam.transform.position + this.cam.transform.forward;
+        transform.rotation = cam.transform.rotation;
+    }
+}
